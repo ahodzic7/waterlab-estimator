3a08017056fd0d82313f5ebcde62f82812698973
Casper Knudsen <ckn16@student.aau.dk>
2020-10-06 14:35:29 +0200

    Cleaned up project structure

--- system_identification/system_identification/Estimation_level.m
+++ system_identification/system_identification/Estimation_level.m
@@ -1,45 +1,65 @@
-clear all;            
+clear all; 
+close all;
 clear path;
-clc;  
+clc; 
+
+% Estimated parameters:
+
+
+% No backflow model:
+% estParams = [0.2008    0.7489    0.0569    0.0165    0.1510];
+
+
 %% ================================================ Prepare data ==============================================
 dataLoad;                                                                       % Load simulation data 
 
 startData = 1; 
-endData = size(g_level,2);
+%endData = size(g_level,2);
+endData = size(no_backflow,2);
 
 Nx = 4;                                                                         % Select section number, i.e. pick number of level sensor data
 if Nx == 6
     h(1:Nx,:) = g_level(1:8:end-1,startData:endData);
 elseif Nx == 4
-    h(1:Nx,:) = g_level(6:12:end,startData:endData);
+    %h(1:Nx,:) = g_level(6:12:end,startData:endData);
+    h(1:Nx,:) = no_backflow(3:1:6,startData:endData);
 elseif Nx == 8
     h(1:Nx,:) = g_level(1:6:end,startData:endData);
 end
 
-Q(1,:) = uConv(g_flow(1,startData:endData),'sTo10m');                           % Select in/outflow
-Q(2,:) = uConv(g_flow(end,startData:endData),'sTo10m');
+% Q(1,:) = uConv(g_flow(1,startData:endData),'sTo10m');                           % Select in/outflow
+% Q(2,:) = uConv(g_flow(end,startData:endData),'sTo10m');
+
+Q(1,:) = no_backflow(9,startData:endData);
+Q(2,:) = no_backflow(7,startData:endData);
+T2 = no_backflow(8,startData:endData);
 
 %% ============================================ Idata object ================================================ 
 Ts_data = 1;                                                                    % [10min] in simulation/control 
 
-input = Q(1,:)';
-output = [h(1:1:end-1,:); Q(2,:)]';
+input = [Q(1,:)' T2'];
+output = [h(1:1:end,:); Q(2,:)]';
 
 data = iddata(output,input,Ts_data);                                            % (y,u,Ts) (order)
 
+data.TimeUnit = 'minutes';
+
 %% ===================================================== Model ============================================
 
-modelName = 'model_cont';
+modelName = 'model_cont_diff_wave_version2';
 Ts_model = 0;                                                                   % 0 - continuous model, 1,2,.. - discrete model 
 order = [size(output,2) size(input,2) Nx];                                      % [Ny Nu Nx] (order)
 
 switch(Nx)                                                                      % select initial parameters
-    case 8
-      p = [0.03, 50, 0.2]; 
-    case 6
-      p = [0.01, 40, 0.2];  
     case 4
-      p = [0.01, 1.5, 0.2]; 
+      p = [0.01, 0.10, 0.002, 0.005, 0.02];
+      %p = [0.01, 10, 0.2, 0.5, 2];
+      %p = [0.2, 0.15, 0.0112, 0.0033, 0.03];
+      %p = [0.02, 2.5, 0.005, -0.75, 1.77];                                          % for nonlinear model
+    case 6
+      p = [0.01, 40, 0.2, 0.1, 1.77];  
+    case 5
+      p = [0.01, 1.5, 0.2, 0.1, 1.77]; 
 end
 
 params = [p, Nx];
@@ -51,8 +71,10 @@ sys_init.TimeUnit = 'minutes';
 sys_init.Parameters(1).Name = 'p1';
 sys_init.Parameters(2).Name = 'p2';
 sys_init.Parameters(3).Name = 'p3';
-sys_init.Parameters(4).Name = 'Nx';
-sys_init.Parameters(4).Fixed = true;                                            % number of sections fixed
+sys_init.Parameters(4).Name = 'p4';
+sys_init.Parameters(5).Name = 'p5';
+sys_init.Parameters(6).Name = 'Nx';
+sys_init.Parameters(6).Fixed = true;                                            % number of sections fixed
 size(sys_init);
 
 sys_init.SimulationOptions.AbsTol = 1e-10;
@@ -60,12 +82,15 @@ sys_init.SimulationOptions.RelTol = 1e-8;
 
 sys_init.SimulationOptions.Solver = 'ode4';                                         % 4th order Runge-Kutte solver - fixed-step size                 
 
-sys_init.Parameters(1).Minimum = 0.001;     sys_init.Parameters(1).Maximum = 0.5;   % Parameter constraints
-sys_init.Parameters(2).Minimum = 0.001;     sys_init.Parameters(2).Maximum = 10000;
-sys_init.Parameters(3).Minimum = 0.001;     sys_init.Parameters(3).Maximum = 0.6;
+% sys_init.Parameters(1).Minimum = 0.001;     sys_init.Parameters(1).Maximum = 0.5;   % Parameter constraints
+% sys_init.Parameters(2).Minimum = 0.001;     sys_init.Parameters(2).Maximum = 10000;
+% sys_init.Parameters(3).Minimum = 0.001;     sys_init.Parameters(3).Maximum = 0.6;
+% sys_init.Parameters(4).Minimum = 0.001;     sys_init.Parameters(4).Maximum = 6;
+% sys_init.Parameters(5).Minimum = 0.001;     sys_init.Parameters(5).Maximum = 2;
 
 for i = 1:Nx
 sys_init.InitialStates(i).Minimum = 0.000001;                                       % States lower bound constraints
+%sys_init.InitialStates(i).Maximum = 0.4; 
 end
 
 %% ============================================= Solver options ============================================
@@ -74,7 +99,7 @@ opt = nlgreyestOptions;
 %Search methods: 'gn' | 'gna' | 'lm' | 'grad' | 'lsqnonlin' | 'auto'
 opt.SearchMethod = 'gna'; 
 opt.Display = 'on';
-opt.SearchOption.MaxIter = 50;
+opt.SearchOption.MaxIter = 150;
 opt.SearchOption.Tolerance = 1e-15;
 
 %% =============================================== Estimation =============================================
@@ -86,7 +111,9 @@ sys_final.Report.Termination
 
 estParams = [sys_final.Parameters(1).Value...
              sys_final.Parameters(2).Value,...
-             sys_final.Parameters(3).Value];
+             sys_final.Parameters(3).Value...
+             sys_final.Parameters(4).Value...
+             sys_final.Parameters(5).Value];
 
 finalStates = sys_final.Report.Parameters.X0;                                       % estimated initial states
 toc
--- system_identification/system_identification/models/free_flow_model.m
+++ system_identification/system_identification/models/free_flow_model.m
@@ -1,20 +1,20 @@
-function [dx, y] = model_cont_diff_wave(t, x, u, p1, p2, p3, p4, p5, Nx, varargin)
+function [dx, y] = free_flow_model(t, x, u, p1, p2, p3, p4, p5, Nx, varargin)
 % Continuous time nlgreyest model for the kinematic wave level-based model. 
 tank_offset = 1;
 dx = zeros(Nx,1);
 y = zeros(Nx+1,1);
 %% State equation
-dx(1) =  p1 * u(1) - p1*p2 * x(1) + p1*p3*x(2)-p1*p4; 
+dx(1) =  p1 * u(1) - p2 * x(1) + p3*x(2)-p4; 
 
 for i = 2:Nx-1 %:Nx-1
-     dx(i) =  p1*p2*x(i-1) - p1*(p2+p3)*x(i) + p1*p3*x(i+1); 
+     dx(i) =  p2*x(i-1) - (p2+p3)*x(i) + p3*x(i+1); 
      %disturbace model:
      %( a1* cos(w*t) + b1* sin(w*t) + a2* cos(2*w*t) + b2* sin(2*w*t)+...
      %    a3* cos(3*w*t) + b3* sin(3*w*t)+ a4* cos(4*w*t) + b4* sin(4*w*t));
 end
 
 % Linear discharge free flow(comment out one)
-dx(Nx) = p1*p2 * x(Nx-1) - p1*p3*x(Nx) + p1*p4 -  p5*(x(Nx));
+dx(Nx) = p2 * x(Nx-1) - p3*x(Nx) + p4 -  p5*(x(Nx));
 % Submerged flow (comment out)
 % dx(Nx) = p1*p2 * x(Nx-1) - p1*p3*x(Nx)+ p1*p4 -  p5*(x(Nx)-u(2)+tank_offset);
 % Offset flow
