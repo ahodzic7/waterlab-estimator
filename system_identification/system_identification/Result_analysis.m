8d380a98772420faa111ef1d2cfeac2b21c53bca
unknown <ckn16@student.aau.dk>
2020-12-03 10:54:59 +0100

    merged

--- system_identification/system_identification/Result_analysis.m
+++ system_identification/system_identification/Result_analysis.m
@@ -8,7 +8,7 @@ for i = 1:1:Nx
     else
         residual = (output(2:end,i)-y_final.OutputData(2:end,i))*100/0.5;
     end
-    plot(0:dataTimeStep:size(output(:,1),1)*dataTimeStep-dataTimeStep-1,residual,'b','LineWidth',1);
+    plot(0:dataTimeStep:size(output(:,1),1)*dataTimeStep-1,residual,'b','LineWidth',1);
     hold on;
     yline(0,'k','LineWidth',1.5);
     residuals = [residuals residual];
@@ -28,18 +28,18 @@ for i = 1:1:Nx
     grid on;
     j = j+1;
 end
-xlabel('Time [s]','interpreter','latex');
-figure
-ax(1) = subplot(size(output,2),1,[2 3 4]);
-histogram(residuals,20,'Normalization','probability');
-hold on
-%leg = legend('Pipe inflow','Location','NorthEast');
-%leg = legend('Tank outflow','Location','NorthEast');
-%set(leg, 'Interpreter', 'latex');
-%ylabel(['[$\%$]'],'interpreter','latex');
-title('Distribution of residuals','interpreter','latex')
-grid on;
-xlabel('Residuals [$\%$]','interpreter','latex');
+% xlabel('Time [s]','interpreter','latex');
+% figure
+% ax(1) = subplot(size(output,2),1,[2 3 4]);
+% histogram(residuals,100,'Normalization','probability');
+% hold on
+% leg = legend('Pipe inflow','Location','NorthEast');
+% leg = legend('Tank outflow','Location','NorthEast');
+% set(leg, 'Interpreter', 'latex');
+% ylabel(['[$\%$]'],'interpreter','latex');
+% title('Distribution of residuals','interpreter','latex')
+% grid on;
+% xlabel('Residuals [$\%$]','interpreter','latex');
 
 %%
 figure
@@ -49,17 +49,9 @@ new_residuals = [0 residuals(:,1)'];
 % end
 histogram(new_residuals,10,'Normalization','probability');
 hold on;
-[muHat,sigmaHat] = normfit(new_residuals)
-x = -30:30;
-y_norm = normpdf(x,muHat,sigmaHat);
+[muHat,sigmaHat] = normfit(residuals(:,1))
+
+x = -0.1:0.01:0.1
+y_norm = normpdf(x,muHat(1),sigmaHat(1));
 plot(x,y_norm);
-lag = 100;
-[xc,lags] = xcorr(new_residuals,Q(2,:),lag,'normalized');
-
-
-figure
-stem(lags,xc,'filled');
-ylim([-1,1]);
-hold on
-figure
-periodogram(new_residuals)
+% lag = 100;
