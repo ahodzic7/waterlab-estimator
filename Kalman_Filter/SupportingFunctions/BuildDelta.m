bc948d41864d10189265a2435212673adcb7a3ac
unknown <ckn16@student.aau.dk>
2020-12-09 12:25:17 +0100

    Fixed errors and added noise_identifaction tool

--- Kalman_Filter/SupportingFunctions/BuildDelta.m
+++ Kalman_Filter/SupportingFunctions/BuildDelta.m
@@ -2,9 +2,9 @@ function Delta = BuildDelta(NumberOfStates, p,DeltaT)
 %BUILDDELTA  Returns the Delta matrix for the two tank topology with
 %NumberOfStates
 
-Delta = [p(4)*DeltaT];
+Delta = [-p(4)*DeltaT];
 Delta = [Delta; zeros(NumberOfStates-3,1)];
-Delta = [Delta; -p(4)*DeltaT];
+Delta = [Delta; p(4)*DeltaT];
 Delta = [Delta; 0];
 end
 
--- Kalman_Filter/model_noise_identification.m
+++ Kalman_Filter/model_noise_identification.m
@@ -1,57 +1,72 @@
-%% Residual plots
+
+%% Calculate one step residuals
+% Setup Model
+NumberOfStates = 5;
+% For lab
+p = [0.0348661973172381,0.0548687811513346,0.000100000000000000,...
+        -0.00169231647536930,0.0342193052314381,0.203718327157626];
+phi = [1,1/4.908738521234052];
+DeltaT = 0.5;
+
+% For Basic Test
+% p = [0.00573303726337542,0.00455712815840905,0.00100679057458217,...
+%     -6.99861868943381e-05,0.00307844320832101,0.00500000000000000];
+% phi = [1,0.00500000000000000];
+% DeltaT = 60;
+
+A = BuildA(NumberOfStates,p,phi,DeltaT);
+B = BuildB(NumberOfStates,p,phi,DeltaT);
+Delta = BuildDelta(NumberOfStates, p,DeltaT);
+
+measurements = [h; T2];
+
+for index = 1:1:size(output,1)-1
+    x_est(index+1,:) = (A*measurements(:,index) + B*input(index,:)' + Delta)';
+end
+residual = (output-x_est);
+
+%Plot
 figure
-residuals = [];
-for i = 1:1:Nx
-    ax(i) = subplot(size(output,2),1,i);
-    if i == Nx
-        residual = (output(2:end,i)-y_final.OutputData(2:end,i));
-    else
-        residual = (output(2:end,i)-y_final.OutputData(2:end,i));
-    end
-    plot(0:dataTimeStep:size(output(:,1),1)*dataTimeStep-1,residual,'b','LineWidth',1);
+for index = 1:1:Nx
+    ax(index) = subplot(size(output,2),1,index);
+    plot(0:dataTimeStep:(size(output(:,1),1)-1)*dataTimeStep,residual(:,index)','b','LineWidth',1);
     hold on;
     yline(0,'k','LineWidth',1.5);
-    residuals = [residuals residual];
-    %hold on
-%     plot(y_final.OutputData(:,i),'r','LineWidth',0.5)
-%     leg = legend('HF Model','Lin. Model','Location','NorthEast');
-%     set(leg, 'Interpreter', 'latex');
-%     set(leg,'color','none');
-    if i == Nx
-        ylabel(['$r_{T2}$' '[$\%$]'],'interpreter','latex');
+    if index == Nx
+        ylabel(['$r_{T2}$'],'interpreter','latex');
     else
-        ylabel(['$r$' num2str(i) '[$\%$]'],'interpreter','latex');
+        ylabel(['$r$' num2str(index)],'interpreter','latex');
     end
-    if i == 1
+    if index == 1
         title('Residuals','interpreter','latex')
     end
     grid on;
-    j = j+1;
 end
-% xlabel('Time [s]','interpreter','latex');
-% figure
-% ax(1) = subplot(size(output,2),1,[2 3 4]);
-% histogram(residuals,100,'Normalization','probability');
-% hold on
-% leg = legend('Pipe inflow','Location','NorthEast');
-% leg = legend('Tank outflow','Location','NorthEast');
-% set(leg, 'Interpreter', 'latex');
-% ylabel(['[$\%$]'],'interpreter','latex');
-% title('Distribution of residuals','interpreter','latex')
-% grid on;
-% xlabel('Residuals [$\%$]','interpreter','latex');
-
-%%
+%% Find residual distribution
+%Assuming normal independant residauls
 figure
-new_residuals = [residuals(:,1)'];
-for i = 2:5
-    new_residuals = [new_residuals residuals(:,i)'];
+state_muHat = [];
+state_sigmaHat = [];
+% Nx = Nx-1;
+for i = 1:1:Nx
+    %Approximate distribution
+    [muHat,sigmaHat] = normfit(residual(:,i));
+    state_muHat = [state_muHat, muHat];
+    state_sigmaHat = [state_sigmaHat, sigmaHat];
+    t = -0.08:0.0001:0.08;
+    pdf = normpdf(t,muHat,sigmaHat)/100;
+    
+    %Plot
+    subplot(Nx,1,i);
+    histogram(residual(:,i),30,'Normalization','probability');
+    hold on;
+    plot(t,pdf);
+    if i == Nx
+        ylabel(['$|r_{T2}|$'],'interpreter','latex');
+    else
+        ylabel(['$|r$' num2str(i) '$|$' ],'interpreter','latex');
+    end
 end
-histogram(new_residuals,100,'Normalization','probability');
-hold on;
-[muHat,sigmaHat] = normfit(residuals(:,1))
 
-x = -0.1:0.01:0.1
-y_norm = normpdf(x,muHat(1),sigmaHat(1));
-plot(x,y_norm);
-% lag = 100;
+state_muHat
+state_sigmaHat
