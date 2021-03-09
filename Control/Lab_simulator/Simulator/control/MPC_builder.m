36b824e6876363a26c1acf2d2cfa4f95846e8d58
AAU117393\Casper and Adis <ckn16@student.aau.dk>
2021-03-09 18:35:16 +0100

    Objective change

--- Control/Lab_simulator/Simulator/control/MPC_builder.m
+++ Control/Lab_simulator/Simulator/control/MPC_builder.m
@@ -39,7 +39,7 @@ Y     = P(2)*g(X(Nxt+ Nxp,:),P(3));
 
 %% =========================================== Objective function ==============================
 % 5000*sumsqr(Gs.*S) + 0.01*sumsqr(Gx.*X(1:Nxt,:)) + 50*sumsqr(Gu.*U);  
-objective = sumsqr(U) + 1*sumsqr(S) + 10*(Kt/dt_MPC)*sumsqr(X(1:Nxt,:) - X_ref); %+ 1*sumsqr(X(1:Nxt,:)); %+ sumsqr(X(1:Nxt,:) - X_ref); %+ sumsqr(S);                          
+objective = 0*sumsqr(U) + 1*sumsqr(S) + 100*(Kt/dt_MPC)*sumsqr(X(1:Nxt,:) - X_ref); %+ 1*sumsqr(X(1:Nxt,:)); %+ sumsqr(X(1:Nxt,:) - X_ref); %+ sumsqr(S);                          
 opti.minimize(objective); 
 
 %% ============================================== Dynamics =====================================
