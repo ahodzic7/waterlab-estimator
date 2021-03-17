c66cf0d24ba3627aa8cb9a31ddbe3b8fe2da0530
unknown <ckn16@student.aau.dk>
2021-03-17 10:46:54 +0100

    Fixing Adis' mistake in the LQR, tsk tsk tsk

--- Control/Lab_Stochastic_MPC_full_system_Linear_DW/SupportingFunctions/make_LQR.m
+++ Control/Lab_Stochastic_MPC_full_system_Linear_DW/SupportingFunctions/make_LQR.m
@@ -1,8 +1,8 @@
     max_deviation_from_mean_x = 1;
-    max_allowed_u = 0.5/60;
+    max_allowed_u = 0.05/60;
     sys = evalin('base','sys');
     A = full(sys.A(dT));
     B = full(sys.B(dT));
-    Q = diag([1/(max_deviation_from_mean_x^2), 0.0001, 0.0001, 0.0001, 0.0001, 1/(max_deviation_from_mean_x^2)]);
-    R = diag([1/max_allowed_u^2 1/max_allowed_u^2]);
-    K = lqr(A,B,Q,R);
+    Q = diag([1/(max_deviation_from_mean_x^2), 0.001, 0.001, 0.001, 0.001, 1/(max_deviation_from_mean_x^2)]);
+    R = diag([1/(max_allowed_u^2) 1/(max_allowed_u^2)]);
+    [K,S,e] = lqrd(A,B,Q,R,dT);
