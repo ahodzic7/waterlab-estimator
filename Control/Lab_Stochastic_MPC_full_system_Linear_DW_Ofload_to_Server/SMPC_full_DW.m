254b822276161774b56ac605e8c3a72a7ca1e2e5
unknown <ckn16@student.aau.dk>
2021-04-14 16:45:45 +0200

    Began to add support for ofloading calculation of MPC to server

---
+++ Control/Lab_Stochastic_MPC_full_system_Linear_DW_Ofload_to_Server/SMPC_full_DW.m
@@ -0,0 +1,88 @@
+function [output]  = SMPC_full_DW(X0,time)
+% define persistent variables
+eml.extrinsic('evalin');
+persistent x_init;
+persistent lam_g;
+persistent OCP;
+persistent Hp;
+persistent warmStartEnabler;
+persistent D_sim;
+persistent X_ref;
+persistent U0;
+persistent sigma_x;
+persistent X_pre;
+persistent sys;
+persistent K;
+% and others
+dT = 10;                 % Sample time in minutes
+simulink_frequency = 2;  % Sampling frequency in seconds
+
+if isempty(lam_g) 
+    % init persistent variables
+    lam_g = 1;
+    x_init = 0.001;
+    
+    % initialize MPC
+    U0 = [3;4.5];
+    X_pre = X0/100;
+    
+    % get optimization problem and warmStartEnabler
+    OCP = evalin('base','OCP');
+    Hp = evalin('base','Hp');
+    D_sim = evalin('base','D_sim');
+    D_sim = D_sim(1:2:3,:);
+    X_ref = evalin('base','X_ref_sim');
+    warmStartEnabler = evalin('base','warmStartEnabler');
+    
+    % initialize CC tube controller
+    make_LQR      % Calculate LQR
+    make_sigma_X  % Precompute sigma_X for chance constraint, Open Loop MPC
+end
+X0 = X0/100;                                                               % Unit convertion from mm to dm
+
+
+%Create forcast from disturbance reference
+time = int64(round(time));
+disturbance = zeros(2,Hp);
+reference = zeros(6,Hp);
+for i=0:1:Hp-1
+    start_index = time+1+i*dT*simulink_frequency;
+    end_index = start_index+dT*simulink_frequency-1;
+    disturbance(:,i+1) = mean(D_sim(:,start_index:end_index),2)/60;
+    reference(1:5:6,i+1) = mean((X_ref(:,start_index:end_index)),2);
+end
+
+
+
+
+% run openloop MPC
+if warmStartEnabler == 1
+    % Parametrized Open Loop Control problem with WARM START
+    [u , S, S_ub, lam_g, x_init] = (OCP(X0,U0,disturbance, lam_g, x_init, dT,reference,sigma_x));
+elseif warmStartEnabler == 0
+    % Parametrized Open Loop Control problem without WARM START 
+    [u , S, S_ub] = (OCP(X0 ,U0, disturbance, dT, reference,sigma_x));
+end
+
+
+
+% create outputs
+u_full = full(u);
+S_full = full(S);
+S_ub_full = full(S_ub);
+lqr_contribution =min(1/60*ones(2,1), max(-1/60*ones(2,1),  K*(X0-X_pre)));
+
+output = [min(sys.U_ub, max(sys.U_lb, u_full(:,1) - lqr_contribution)) ; S_full(:,1)]*60;           % Saturate the outputs
+output = [output; X_ref(:,time+1)*100; S_ub_full(:,1)];
+
+% Set vairables for next iteration
+U0 = u_full(:,1);
+X_pre = full(sys.F_system(X0, u_full(:,1), disturbance(:,1), dT));
+for i =1:5:6
+    if X_pre(i) > sys.X_ub(i)
+        X_pre(i) = sys.X_ub(i);
+    elseif X_pre(i) < sys.X_lb(i)
+        X_pre(i) = sys.X_lb(i);
+    end
+end
+end
---
+++ Control/Lab_Stochastic_MPC_full_system_Linear_DW_Ofload_to_Server/SupportingFunctions/plot_MPC.m
@@ -0,0 +1,107 @@
+    figure(1)
+    clf(1)
+    elapsed_time = 0:step-1;
+    future_time = elapsed_time(end):(elapsed_time(end)+Hp);
+    
+    subplot(5,1,1)
+    title('Tank States');
+    hold on
+    % Time elapsed in simulation
+    plot(elapsed_time, X_sim_num(1,1:step),'b', 'DisplayName','Tank1')
+    hold on
+    plot(elapsed_time, X_sim_num(6,1:step),'g', 'DisplayName','Tank2')
+    hold on
+    % Current step
+    plot(elapsed_time(end), X_sim_num(1,step),'r*');
+    hold on
+    plot(elapsed_time(end), X_sim_num(6,step),'r*');
+    hold on
+    % Future predicitions
+    plot(future_time ,X_predict_num(1,:),'b--');
+    hold on
+    plot(future_time ,X_predict_num(6,:),'g--');
+    hold on
+    xlim([0,N+Hp])
+    hold off
+    legend show
+    
+    subplot(5,1,2)  
+    title('Control inputs');
+    hold on
+    plot(elapsed_time, U_sim_num(1,1:step),'b')
+    hold on
+    plot(elapsed_time, U_sim_num(2,1:step),'g')
+    hold on
+    for i = 1:2
+    plot(elapsed_time(end), U_sim_num(i,step),'r*');
+    hold on
+    end
+    plot(future_time ,[U_sim_num(1,step),U_out_num(1,:)],'b--');
+    hold on
+    plot(future_time ,[U_sim_num(2,step),U_out_num(2,:)],'g--');
+    hold off
+    xlim([0,N+Hp])
+    
+    subplot(5,1,3)
+    title('Slack Variables');
+    hold on
+    plot(elapsed_time, S_sim_num(1,1:step),'b')
+    hold on
+    plot(elapsed_time, S_sim_num(2,1:step),'g')
+    hold on
+    for i = 1:2
+    plot(elapsed_time(end), S_sim_num(i,step),'r*');
+    hold on
+    end
+    plot(future_time ,[S_sim_num(1,step),S_out_num(1,:)],'b--');
+    hold on
+    plot(future_time ,[S_sim_num(2,step),S_out_num(2,:)],'g--');
+    hold off
+    xlim([0,N+Hp])
+    
+    subplot(5,1,4)
+    title('CC-Slack Variables');
+    hold on
+    plot(elapsed_time, S_ub_sim_num(1,1:step),'b')
+    hold on
+    plot(elapsed_time, S_ub_sim_num(2,1:step),'g')
+    hold on
+    for i = 1:2
+    plot(elapsed_time(end), S_ub_sim_num(i,step),'r*');
+    hold on
+    end
+    plot(future_time ,[S_sim_num(1,step),S_ub_out_num(1,:)],'b--');
+    hold on
+    plot(future_time ,[S_sim_num(2,step),S_ub_out_num(2,:)],'g--');
+    hold off
+    xlim([0,N+Hp])
+    
+    subplot(5,1,5)
+    title('Disturbance');
+    hold on
+    plot(disturbance(1,:),'k--')
+    hold on
+    plot(dist_forcast(1,:),'k')
+    hold on
+    plot(disturbance(2,:),'r--')
+    hold on
+    plot(dist_forcast(2,:),'r')
+    hold off
+    xlim([0,N+Hp])
+    
+    
+    
+    figure(2)
+    clf(2)
+    for i=2:5
+   % Time elapsed in simulation
+    plot(elapsed_time, X_sim_num(i,1:step),'DisplayName',['Pipe ',num2str(i-1)])
+    hold on
+    % Future predicitions
+    plot(future_time ,X_predict_num(i,:),'--');
+    hold on
+    end
+    title('Pipe states');
+    hold off
+    legend show
+    
