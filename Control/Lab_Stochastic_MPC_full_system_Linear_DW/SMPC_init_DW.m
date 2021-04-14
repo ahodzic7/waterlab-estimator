c69b691c0d60859caed0964bab8e3a6fa8afb573
AAU117393\Casper and Adis <ckn16@student.aau.dk>
2021-04-14 16:45:02 +0200

    SMPC_init changed

--- Control/Lab_Stochastic_MPC_full_system_Linear_DW/SMPC_init_DW.m
+++ Control/Lab_Stochastic_MPC_full_system_Linear_DW/SMPC_init_DW.m
@@ -25,12 +25,13 @@ U_ub   = [8.3;16]/60;                      % input bounds
 U_lb   = [3.4;6]/60;
 dU_ub  = [4.5;4.5]/60;
 dU_lb  = [-4.5;-4.5]/60;
-Xt_ub  = 7.02;                          % state bounds tank
+Xt_1_ub  = 7.02;                          % state bounds tank
+Xt_2_ub  = 6.43;  
 Xt_lb  = 1.8;
 Xp_ub  = 0.5;                           % state bounds pipes                          
 Xp_lb  = -10;
 % Combine into system bounds
-X_ub   = [Xt_ub, Xp_ub*ones(1,nP), Xt_ub]'; 
+X_ub   = [Xt_1_ub, Xp_ub*ones(1,nP), Xt_2_ub]'; 
 X_lb   = [Xt_lb, Xp_lb*ones(1,nP), Xt_lb]'; 
 %% ========================================= Optimization variables ============================
 X  = opti.variable(nS,Hp+1);            % state - volume 
