eeb2d2d6baac0dc8bf5fac9077b48af82863bba4
AAU117393\Casper and Adis <ckn16@student.aau.dk>
2021-04-16 13:52:27 +0200

    SMPC server implementation

--- Control/Lab_Stochastic_MPC_full_system_Linear_DW_Ofload_to_Server/SMPC_init_DW.m
+++ Control/Lab_Stochastic_MPC_full_system_Linear_DW_Ofload_to_Server/SMPC_init_DW.m
@@ -176,6 +176,6 @@ elseif warmStartEnabler == 0
     OCP = opti.to_function('OCP',{X0,U0,D,T,Reference,sigma_X},{U,S,S_ub},{'x0','u0','d','dt','ref','sigma_x'},{'u_opt','s_opt','S_ub_opt'});
 end
 
-load('.\Lab_Stochastic_MPC_full_system_Linear_DW\X_ref_sim.mat');
-load('.\Lab_Stochastic_MPC_full_system_Linear_DW\D_sim.mat');
+load('.\Lab_Stochastic_MPC_full_system_Linear_DW_Ofload_to_Server\X_ref_sim.mat');
+load('.\Lab_Stochastic_MPC_full_system_Linear_DW_Ofload_to_Server\D_sim.mat');
 
